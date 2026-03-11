673c5440602405a51d5631d75c1426905df25123
root <root@admin.hypercoding.dev>
2026-03-11 14:29:50 +0700

    feat: update download mods and fix errors

--- latest/overlay/etc/container.d/25-chown.sh
+++ latest/overlay/etc/container.d/25-chown.sh
@@ -2,12 +2,12 @@
 
 if [[ "${DST_SKIP_STATE_CHOWN}" != "true" ]]; then
     echo "> chown state directory"
-    find /var/lib/game \( \! -user steam -o \! -group steam \) -print0 | xargs -0 -r chown steam:steam
+    find /var/lib/game \( \! -user steam -o \! -group steam \) -print0 | xargs -0 -r chown steam:steam 2>/dev/null || true
 fi
 
 if [[ "${DST_SKIP_GAME_CHOWN}" != "true" ]]; then
     echo "> chown game directory"
-    find /usr/share/game \( \! -user steam -o \! -group steam \) -print0 | xargs -0 -r chown steam:steam
+    find /usr/share/game \( \! -user steam -o \! -group steam \) -print0 | xargs -0 -r chown steam:steam 2>/dev/null || true
 fi
 
 true
